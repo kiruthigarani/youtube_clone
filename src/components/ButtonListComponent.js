2370f6a33cd21fbae33e08586238572489a14992
kiruthigaranik <kiruthigaranik@gmail.com>
2026-02-26 18:55:29 +0530

    video listing and styling

--- src/components/ButtonListComponent.js
+++ src/components/ButtonListComponent.js
@@ -2,7 +2,8 @@ import React from 'react'
 import Button from './Button';
 
 const ButtonListComponent = () => {
-  const list = ["All", "Music", "Gaming", "Live", "News", "Sports", "Comedy", "Cooking", "Education", "Technology", "Travel", "Fashion", "Health", "Fitness", "Animals", "Science", "Art", "History", "Business", "Finance"];
+  // const list = ["All", "Music", "Gaming", "Live", "News", "Sports", "Comedy", "Cooking", "Education", "Technology", "Travel", "Fashion", "Health", "Fitness", "Animals", "Science", "Art", "History", "Business", "Finance"];
+    const list = ["All", "Music", "Gaming", "Live", "News", "Sports", "Comedy", "Cooking", "Education", "Technology",  "Science", "Art", "History", "Business", "Finance"];
   return (
     <div className="flex p-4">
       {
--- src/components/VideoComponent.js
+++ src/components/VideoComponent.js
@@ -1,23 +1,34 @@
-import React, {useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 import { apiURL } from '../utils/Constant';
+import VideoCard from './VideoCard';
+
 const VideoComponent =  () => {
 
+  const [json, setJson] = useState(null);
   useEffect( () =>{
    getResponse();
-  
+   
   }, []);
 
   const getResponse =async ()=>{
   const data = await fetch(apiURL);
   const json = await data.json();
-  console.log(json);
+  setJson(json.items);
+  
+ 
   }
+  
 
   return (
-    <div>
-   
+    <div className='flex flex-wrap'>
+      {
+        json && json.map((video)=>{
+          return <VideoCard video={video} key={video.id}/>
+        })
+      }
+     
     </div>
   )
 }
 
-export default VideoComponent
+export default VideoComponent;
